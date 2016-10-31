7dd17d4f686a947b7b947aeb7f5188acf86061ff
Robin De Herdt <rdh_robin@hotmail.com>
2016-10-31 10:33:22 +0100

    cronjob command conditional fix

--- app/Console/Commands/MyJob.php
+++ app/Console/Commands/MyJob.php
@@ -44,26 +44,29 @@ class MyJob extends Command
 
         // http://stackoverflow.com/questions/24208502/laravel-orderby-relationship-count
         // Tel alle stemmen bij de creaties die nu deelnemen, sorteer ze dan en output 1 winnaar
-        $orderedCreations = Creation::with('votes')->where('isParticipating', true)->get()->sortBy(function($creation)
+        if(Creation::where('isParticipating', true)->count())
         {
-            return $creation->votes->count();
-        }, SORT_REGULAR, true);
+            $orderedCreations = Creation::with('votes')->where('isParticipating', true)->get()->sortBy(function($creation)
+            {
+                return $creation->votes->count();
+            }, SORT_REGULAR, true);
 
-        $mostVotedCreation = $orderedCreations->first();
+            $mostVotedCreation = $orderedCreations->first();
 
-        $winner = new Winner();
-        $winner->creation_id = $mostVotedCreation->id;
-        $winner->save();
+            $winner = new Winner();
+            $winner->creation_id = $mostVotedCreation->id;
+            $winner->save();
 
-        echo "Winner picked\n";
+            echo "Winner picked\n";
 
-        // Zet als dit werkt bij alle huidige deelnames isParticipating op false
-        foreach ($orderedCreations as $creation) {
-            $creation->isParticipating = false;
-            $creation->save();
-        }
+            // Zet als dit werkt bij alle huidige deelnames isParticipating op false
+            foreach ($orderedCreations as $creation) {
+                $creation->isParticipating = false;
+                $creation->save();
+            }
+
         echo "Soft deleted creations for this cycle\n";
-
+        }
         // Zet gebruikte contestimage op true
         $contestimage = Contestimage::where('isUsed', false)->first();
         $contestimage->isUsed = true;
