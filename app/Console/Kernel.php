8dd4d05895d311f3080f313a7a6486b837771b61
Robin De Herdt <rdh_robin@hotmail.com>
2016-11-03 14:09:01 +0100

    Cronjob interval ingesteld

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('do:nextweek')->hourly();
+        $schedule->command('do:nextweek')->weekly();
         $schedule->command('do:dailymail')->daily();
     }
 
